48bb736a300e04052fbbaa04169f00de0a79733e
kareemrb27 <kareemrb27@gmail.com>
2026-01-19 23:21:04 -0500

    Add Netlify Identity Widget Script

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </head>
       <body
         className={cn(
           outfit.variable,
